18cf1f9013e86a3d4cb49110bcee3f30cc2a995a
Axel Brisse <axel.brisse@gmail.com>
2024-11-05 18:39:26 +0100

    .

--- src/quadrant.rs
+++ src/quadrant.rs
@@ -1,9 +1,3 @@
-import os
-import re
-import sys
-
-CONFIG_FILE = "Cargo.toml"
-TEMPLATE = """\
 macro_rules! read {
     ( $t:ty ) => {
         {
@@ -36,28 +30,22 @@ macro_rules! read {
 }
 
 fn main() {
-
+    let x = read!(i32);
+    let y = read!(i32);
+    println!(
+        "{}",
+        if y > 0 {
+            if x > 0 {
+                1
+            } else {
+                2
+            }
+        } else {
+            if x < 0 {
+                3
+            } else {
+                4
+            }
+        }
+    );
 }
-"""
-
-assert len(sys.argv) == 2
-executable = sys.argv[1]
-assert re.fullmatch(r"\w+", executable)
-filename = f"src/{executable}.rs"
-
-toml = open(CONFIG_FILE).read()
-assert filename not in toml
-
-bin_idx = toml.find("[[bin]]")
-start = toml[:bin_idx]
-
-bins = list(map(str.strip, toml[bin_idx:].split("\n\n")))
-bins.append(f'''[[bin]]
-name = "{executable}"
-path = "{filename}"''')
-bins.sort()
-
-print(start + "\n\n".join(bins), file=open(CONFIG_FILE, "w"))
-os.makedirs(os.path.dirname(filename), exist_ok=True)
-print(TEMPLATE, file=open(filename, "w"))
-os.system(f"code {filename}")
--- src/twostones.rs
+++ src/twostones.rs
@@ -1,9 +1,3 @@
-import os
-import re
-import sys
-
-CONFIG_FILE = "Cargo.toml"
-TEMPLATE = """\
 macro_rules! read {
     ( $t:ty ) => {
         {
@@ -36,28 +30,5 @@ macro_rules! read {
 }
 
 fn main() {
-
+    println!("{}", if read!(u64) & 1 == 0 { "Bob" } else { "Alice" });
 }
-"""
-
-assert len(sys.argv) == 2
-executable = sys.argv[1]
-assert re.fullmatch(r"\w+", executable)
-filename = f"src/{executable}.rs"
-
-toml = open(CONFIG_FILE).read()
-assert filename not in toml
-
-bin_idx = toml.find("[[bin]]")
-start = toml[:bin_idx]
-
-bins = list(map(str.strip, toml[bin_idx:].split("\n\n")))
-bins.append(f'''[[bin]]
-name = "{executable}"
-path = "{filename}"''')
-bins.sort()
-
-print(start + "\n\n".join(bins), file=open(CONFIG_FILE, "w"))
-os.makedirs(os.path.dirname(filename), exist_ok=True)
-print(TEMPLATE, file=open(filename, "w"))
-os.system(f"code {filename}")
